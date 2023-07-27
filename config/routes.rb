0160c59fc702017a04b925b9bb71d108f70d08d9
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-07-27 12:58:51 +0000

    viewの訂正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index]
   resources :books, only: [:new, :index, :show, :create]
-  get "homes/about" => "homes#about"
+  get "home/about" => "homes#about", as: 'about'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
