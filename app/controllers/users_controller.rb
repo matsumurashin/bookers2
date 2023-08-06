59a8a5a59dadbcb0c8c4690ca519288fe66a332d
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-08-06 09:03:07 +0000

    users/showレイアウト作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @books = @user.books
     @book = Book.new
   end
