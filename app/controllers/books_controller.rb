a1383a034db1444c736b3d17f544635e0003d60e
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-08-09 14:08:35 +0000

    validation途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,16 @@ class BooksController < ApplicationController
       @books = Book.all
       render :index
     end
-  end
+
+    def create
+      @book_val = Book.new(book_params)
+      @book_val.user_id = current_user.id
+      if @book_val.save
+        redirect_to user_path(params[:id])
+      else
+        render :new
+      end
+    end
 
   def show
     @book_new = Book.new
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+   validates :name, presence: true
+   validates :email, presence: true
+   validates :password, presence: true
+
 end
